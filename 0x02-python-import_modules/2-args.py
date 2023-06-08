78f012734ec3695eee087de764c4fd80194a247a
farahahmeddeen <farahahmeddeen1995@gmail.com>
2023-06-08 03:33:36 -0700

    task 2 import

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import agrv
+if __name__ == "__main__":
+    fh = len(argv) - 1
+    if fh == 0:
+        print("0 arguments.")
+    elif fh == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(fh))
+    for j in range(1, fh + 1):
+        print("{}:{}".format(j, argv[j]))
+
