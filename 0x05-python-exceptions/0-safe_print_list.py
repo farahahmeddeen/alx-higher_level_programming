9b2f8b37c117d34909586946fdcb30850009bfe0
farahahmeddeen <farahahmeddeen1995@gmail.com>
2023-06-26 02:07:51 -0700

    task 0 exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[fh], end="")
             count = count + 1
     except IndexError:
-        break
+        None
     print()
     return (count)
